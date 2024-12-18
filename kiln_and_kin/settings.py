fe650b1e4fc3014ec6c31249d2be34e2fe3042de
Kate Mcguane <katemcguane@hotmail.com>
2024-12-18 23:35:18 +0000

    Troubleshoot URL issue

--- kiln_and_kin/settings.py
+++ kiln_and_kin/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +90,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = ''  # temporary url
+LOGIN_REDIRECT_URL = '/success'  # temporary url
 
 WSGI_APPLICATION = 'kiln_and_kin.wsgi.application'
 
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'kiln_and_kin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
