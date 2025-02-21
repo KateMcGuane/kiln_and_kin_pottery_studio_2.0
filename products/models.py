2c728c035a24ab76afbf1f6729630fd4a42a189a
KateMcGuane <katemcguane@hotmail.com>
2025-02-21 17:38:27 +0000

    Create Category & Product models. Adjust fields layout for clear correspondence between json files and models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    
+    material = models.CharField(max_length=100, null=True, blank=True)
+    glaze_type = models.CharField(max_length=100, null=True, blank=True)
+    colour = models.CharField(max_length=50, null=True, blank=True)
+    
+    # Dimensions added directly, revise for scaleability
+    height = models.CharField(max_length=20, null=True, blank=True)  # Stored as string to include units
+    opening_diameter = models.CharField(max_length=20, null=True, blank=True)
+    diameter_at_widest_point = models.CharField(max_length=20, null=True, blank=True)
+    base_diameter = models.CharField(max_length=20, null=True, blank=True)
+
+    # Stored as string to include units
+    weight = models.CharField(max_length=20, null=True, blank=True)
+    capacity = models.CharField(max_length=20, null=True, blank=True)
+    
+    # Image added directly, revise for multiple images & scaleability
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
