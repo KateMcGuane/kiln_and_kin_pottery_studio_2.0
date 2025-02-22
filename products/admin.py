d7a72ec8657c6c1de2db408188966c2a3caecfe6
KateMcGuane <katemcguane@hotmail.com>
2025-02-22 11:08:12 +0000

    Add meta data to category model; Customise admin: ProductAdmin & CategoryAdmin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,34 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'stock',
+        'material',
+        'glaze_type',
+        'colour',
+        'height',
+        'opening_diameter',
+        'diameter_at_widest_point',
+        'base_diameter',
+        'weight',
+        'capacity',
+        'image',
+    )
+    
+    ordering = ('sku',)
+    
+class CategoryAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
