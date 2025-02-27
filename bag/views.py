cf42ee720ef84a1680b0f05e51c687b7bcca9538
KateMcGuane <katemcguane@hotmail.com>
2025-02-27 19:13:51 +0000

    Add adjust_bag view & connect url

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
 
 def view_bag(request):
-    """ A view that renders the bag contents page """
+    """A view that renders the bag contents page"""
 
     return render(request, 'bag/bag.html')
 
@@ -12,7 +12,10 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """Add a quantity of the specified product to the shopping bag"""
 
-    quantity = int(request.POST.get('quantity'))
+    try:
+        quantity = int(request.POST.get('quantity'))
+    except ValueError:
+        return HttpResponse(status=400)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
@@ -23,3 +26,18 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id, None)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
