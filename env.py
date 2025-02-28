62d67b4de6509f29685e78284c5e2f66431e46c3
KateMcGuane <katemcguane@hotmail.com>
2025-02-28 11:32:10 +0000

    Conditionally set debug

--- env.py
+++ env.py
@@ -1,5 +1,7 @@
 import os
 
-SECRET_KEY = (
-	'django-insecure-%sj)tp!-)p8r^sr-4#ls5nnnsd#ia3hsq!#_d5!=qbhew#7_p+'
+os.environ.setdefault(
+  'SECRET_KEY',
+  'django-insecure-%sj)tp!-)p8r^sr-4#ls5nnnsd#ia3hsq!#_d5!=qbhew#7_p+'
 )
+os.environ.setdefault('DEVELOPMENT', '1')
