7a34b76752c70106056d1149b3690212b1592fc2
KateMcGuane <katemcguane@hotmail.com>
2025-03-19 22:21:13 +0000

    Add stripe elements with customised js & css

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R4ROo4Rnzy3bZJreg0jwDuC8gmQ43JWlt6uPflUacwGVCtksilqJoE5Nc0dykpkVDcti9vti9qDw8SOsTGqVb8j00CNlIQVE2',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
